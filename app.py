9cd6ffbf08e769b98bbcced55dce189ee0df586b
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2022-07-19 10:31:32 +0300

    print version on startup

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import telegram_bot
 import schedule
 import time
 import threading
+import os.path
 
 def scheduler():
     schedule.every().day.at("03:00").do(telegram_bot.update_workout_library)
@@ -10,6 +11,13 @@ def scheduler():
         time.sleep(1)
 
 if __name__ == '__main__':
+    # print version
+    version_file = 'git_commit_version.txt'
+    if os.path.isfile(version_file):
+        with open(version_file) as f:
+            version = f.readline().strip()
+            print('workout_bot ' + version)
+
     scheduleThread = threading.Thread(target=scheduler)
     scheduleThread.daemon = True
     scheduleThread.start()
