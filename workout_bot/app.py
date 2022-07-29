b9418d716ae2ab35163adbc2607fa31e09274fa0
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2022-07-29 11:20:36 +0300

    restructured app.py

--- workout_bot/app.py
+++ workout_bot/app.py
@@ -1,11 +1,20 @@
-import telegram_bot
-import schedule
+"""
+Telegram bot for workouts app entry point.
+"""
+
 import time
 import threading
-import os.path
+from pathlib import Path
+import schedule
+
+from telegram_bot import telegram_bot
+
+VERSION_FILE_NAME = 'git_commit_version.txt'
 
 
 def scheduler():
+    "Schedules google table updates daily at 3 a.m."
+
     schedule.every().day.at("03:00").do(telegram_bot.update_workout_library)
     while True:
         schedule.run_pending()
@@ -14,9 +23,9 @@ def scheduler():
 
 if __name__ == '__main__':
     # print version
-    version_file = 'git_commit_version.txt'
-    if os.path.isfile(version_file):
-        with open(version_file) as f:
+    config_file = Path(VERSION_FILE_NAME)
+    if config_file.is_file():
+        with open(VERSION_FILE_NAME, encoding="utf-8") as f:
             version = f.readline().strip()
             print('workout_bot ' + version)
 
