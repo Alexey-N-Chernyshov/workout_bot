d45c6b7af944487fe03fa2cb73e2c608e97eb742
Alexey Chernyshov <alexey.n.chernyshov@gmail.com>
2022-11-25 16:08:32 +0300

    test (table_management): all tests added for table management

--- tests/behavioral/test_table_management.py
+++ tests/behavioral/test_table_management.py
@@ -3,7 +3,10 @@ Tests related to admin table management.
 """
 
 from workout_bot.data_model.users import UserAction
-from workout_bot.view.tables import get_all_tables_message
+from workout_bot.view.tables import (
+    get_all_tables_message, get_table_name_message
+)
+from workout_bot.view.utils import escape_text
 from workout_bot.controllers.table_management import (
     QUERY_ACTION_SWITCH_PAGE, QUERY_ACTION_CHOOSE_TABLE, InlineKeyboardData
 )
@@ -83,9 +86,64 @@ async def test_table_management_admin_cancel_adds_table(test_table_management):
     alice.assert_user_action(UserAction.ADMIN_TABLE_MANAGEMENT)
 
 
-# TODO test_admin_adds_table_link()
+async def test_admin_adds_table_link(test_table_management):
+    """
+    Given: Admin is in ADMIN_ADDING_TABLE state.
+    When: Admin sends valid link to table
+    Then: Admin is asked to choose pages.
+    """
 
-# TODO test_admin_changes_table()
+    alice = test_table_management.users[0]
+    alice.set_user_action(UserAction.ADMIN_ADDING_TABLE)
+    table_id = "1x2DpoqS9lxUNNWKf5hp4VhHWblWZm-mTTu5I5L3jhtw"
+
+    await alice.send_message("https://docs.google.com/spreadsheets/d/" +
+                             table_id)
+
+    expected = "Добавление таблицы\n"
+    expected += get_table_name_message(None, table_id)
+    expected += "id: " + escape_text(table_id) + "\n"
+    expected += "\n"
+    expected += "Отметьте страницы с тренировками"
+    alice.expect_answer(expected)
+    alice.expect_answer("Управление таблицами")
+    alice.expect_no_more_answers()
+    alice.assert_user_action(UserAction.ADMIN_TABLE_MANAGEMENT)
+
+
+async def test_admin_adds_table_invalid_link(test_table_management):
+    """
+    Given: Admin is in ADMIN_ADDING_TABLE state.
+    When: Admin sends invalid link to table
+    Then: Admin is shown error message
+    """
+
+    alice = test_table_management.users[0]
+    alice.set_user_action(UserAction.ADMIN_ADDING_TABLE)
+
+    await alice.send_message("invalid link")
+
+    alice.expect_answer("Ошибка при загрузке таблицы")
+    alice.expect_answer("Введите ссылку на таблицу")
+    alice.expect_no_more_answers()
+    alice.assert_user_action(UserAction.ADMIN_ADDING_TABLE)
+
+
+async def test_admin_changes_table(test_table_management):
+    """
+    Given: Admin is in ADMIN_TABLE_MANAGEMENT state.
+    When: Admin sends change table message.
+    Then: Select table to change is shown.
+    """
+
+    alice = test_table_management.users[0]
+    alice.set_user_action(UserAction.ADMIN_TABLE_MANAGEMENT)
+
+    await alice.send_message("Изменить таблицу")
+
+    alice.expect_answer("Выберите таблицу для редактирования")
+    alice.expect_no_more_answers()
+    alice.assert_user_action(UserAction.ADMIN_TABLE_MANAGEMENT)
 
 
 async def test_update_tables(test_table_management):
@@ -172,4 +230,59 @@ async def test_admin_adds_page(test_table_management):
     alice.assert_user_action(UserAction.ADMIN_TABLE_MANAGEMENT)
     alice.expect_no_more_answers()
 
-# TODO test_admin_chooses_table_to_change()
+
+async def test_admin_removes_page(test_table_management):
+    """
+    Given: Admin in ADMIN_TABLE_MANAGEMENT state and inline keyboard shown.
+    Page has been added.
+    When: Admin presses inline keyboard with page.
+    Then: Page is deleted from the table.
+    """
+
+    alice = test_table_management.users[0]
+    table = test_table_management.workout_tables[0]
+    table_id = table.table_id
+    assert len(table.pages) == 2
+    pages = list(table.pages.keys())
+    page_to_delete = pages[0]
+    page1 = pages[1]
+
+    data = InlineKeyboardData(QUERY_ACTION_SWITCH_PAGE, page_to_delete) \
+        .encode()
+    message_text = "Добавление таблицы\n"
+    message_text += "New table\n"
+    message_text += f"id: {table_id}\n"
+    await alice.press_inline_button(message_text, data)
+
+    updated_table = test_table_management.data_model.workout_table_names \
+        .get_tables()[table_id]
+    assert len(updated_table) == 1
+    assert page1 in updated_table
+    alice.assert_user_action(UserAction.ADMIN_TABLE_MANAGEMENT)
+    alice.expect_no_more_answers()
+
+
+async def test_admin_chooses_table_to_change(test_table_management):
+    """
+    Given: Admin is in ADMIN_TABLE_MANAGEMENT state and inline keyboard shown.
+    When: Admin presses inline keyboard with table to change chosen.
+    Then: Changing table message is shown .
+    """
+
+    alice = test_table_management.users[0]
+    table_id = test_table_management.workout_tables[1].table_id
+    table_name = test_table_management.workout_tables[1].table_name
+
+    data = InlineKeyboardData(QUERY_ACTION_CHOOSE_TABLE, table_id) \
+        .encode()
+    message_text = "Выберите таблицу для редактирования"
+    await alice.press_inline_button(message_text, data)
+
+    expected = "Редактирование таблицы\n"
+    expected += get_table_name_message(table_name, table_id)
+    expected += "id: " + escape_text(table_id) + "\n"
+    expected += "\n"
+    expected += "Отметьте страницы с тренировками"
+    alice.expect_answer(expected)
+    alice.assert_user_action(UserAction.ADMIN_TABLE_MANAGEMENT)
+    alice.expect_no_more_answers()
